d3aefeb00efeffc8255f2a48cf9f09f4ebfaa8b1
amirfarzadnilizadeh <a.farzadnilizadeh@gmail.com>
2021-06-01 21:40:21 +0430

    make AI path

--- Assets/Scripts/Path.cs
+++ Assets/Scripts/Path.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[ExecuteInEditMode]
 public class Path : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log(transform.childCount);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        for (var i = 0; i < transform.childCount; i++)
+        {
+            //Draw line between Pints
+          if(i > 0){
+           Debug.DrawLine(transform.GetChild(i).position, transform.GetChild(i-1).position, Color.yellow);
+             //fix position Point
+           transform.GetChild(i).LookAt(transform.GetChild(i-1).position);
+           }
+            //rename cube to Point i
+            transform.GetChild(i).name = "Point" + i;
+        }
     }
 }
